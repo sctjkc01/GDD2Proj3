4fc4eefb1c93b59846c4b3d8e520f69e3de9886f
Daniel Law <djl1005@rit.edu>
2014-11-09 13:40:32 -0500

    Small changes to tower

--- Towers/Assets/scripts/Tower.cs
+++ Towers/Assets/scripts/Tower.cs
@@ -3,8 +3,25 @@ using System.Collections;
 
 public class Tower : MonoBehaviour {
 
+	/// <summary>
+	/// The _mods only GameObject for now
+	/// </summary>
+	private GameObject _mods[4];
+
+	/// <summary>
+	/// The time between shots.
+	/// </summary>
+	public float timeBetweenShots;
+
+
+	public float dammage;
+
+	private float _timer;
+
+
 	// Use this for initialization
 	void Start () {
+
 	
 	}
 	
