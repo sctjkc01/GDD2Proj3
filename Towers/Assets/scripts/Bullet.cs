ade3fe91e3d77339ab7cb5b7a7c9613329f4e89a
Daniel Law <djl1005@rit.edu>
2014-11-09 13:55:17 -0500

    Added bullet

---
+++ Towers/Assets/scripts/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	private float _damage;
+
+	private float _speed;
+
+	
+	public float Damage
+	{
+		get { return _damage; }
+		set { _damage = value; }
+	}
+
+	public float Speed
+	{
+		get { return _speed; }
+		set { _speed = value; }
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+		// find nereast enemy and traget it.
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	void onColisiionEneter(Collision obj)
+	{
+
+		//when colides with enemy ....
+
+	}
+
+}
