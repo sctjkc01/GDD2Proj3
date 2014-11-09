2dde9334eedc311eb8b06640644891ce9f5ed574
Haplicity <fu_charles@yahoo.com>
2014-11-09 14:42:35 -0500

    Enemy prefab

---
+++ Towers/Assets/scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float health;
+	public float speed;
+
+   	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
