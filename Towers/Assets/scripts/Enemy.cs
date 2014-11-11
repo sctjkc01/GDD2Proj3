26b84b5bf76bd1fc098c5363bc2246e8c1884f0c
Haplicity <fu_charles@yahoo.com>
2014-11-11 11:48:03 -0500

    Untested Enemy stuff

--- Towers/Assets/scripts/Enemy.cs
+++ Towers/Assets/scripts/Enemy.cs
@@ -3,16 +3,49 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-	public float health;
-	public float speed;
+	public float health = 20;
+    public float listIndex = 0;
+
+    public static PathTile start;
+    public static PathTile end;
+
+    public Vector3 movement;
+
+    public List<PathTile> tileList;
 
    	// Use this for initialization
-	void Start () {
-	
+	void Start()
+    {
+        GameObject.getComponent<TileMap>().findPath(start, end, tileList);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update() 
+    {
+        movement = transform.position - tileList[listIndex].transform.position;
+        transform.translate(movement * Time.deltaTime);
+        if (health < 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (transform.positon == tileList[listIndex].transform.position)
+        {
+            if (listIndex == tileList.length)
+            {
+                //make player lose life here
+                Destroy(this.gameObject);
+            }
+
+            else
+            {
+                listIndex++;
+            }
+        }
 	}
+
+    void takeDamage(float dmg)
+    {
+        health -= dmg;
+    }
 }
---
+++ Towers/Assets/scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public Transform enemy;
+    public PathTile start;
+    public float counter;
+
+    // Use this for initialization
+    void Start() 
+    {
+    }
+
+    void roundStart() 
+    {
+        InvokeRepeating("SpawnEnemies", 0.1f, 0.0f);
+    }
+
+    void SpawnEnemies() 
+    {
+        Vector3 location = start.transform.position;
+        // Debug.Log("I want to see an enemy here: " + location);
+        Instantiate(enemy, location, Quaternion.identity);
+        counter++;
+
+        if (counter > 20)
+        {
+            CancelInvoke();
+        }
+    }
+}
