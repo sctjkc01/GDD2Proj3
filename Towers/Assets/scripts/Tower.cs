d0ce1e5e59257b8018884e6afe6bc82331d44c8c
Daniel Law <djl1005@rit.edu>
2014-11-11 15:55:05 -0500

    Tower junk
    towers now have a trigger that adds enemies to list and so on.
    note for placement to work Properly I had to add two methods to tower
    which must be called at the start and end of a round respectivly

--- Towers/Assets/scripts/Tower.cs
+++ Towers/Assets/scripts/Tower.cs
@@ -1,32 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Tower : MonoBehaviour {
 
 	/// <summary>
 	/// The _mods only GameObject for now
 	/// </summary>
-	public GameObject[] _mods = new GameObject[4];
+	public GameObject[] mods = new GameObject[4];
+
+	public List<GameObject> enemies = new List<GameObject>();
+
+	private Collider colider;
 
 	/// <summary>
 	/// The time between shots.
 	/// </summary>
-	public float timeBetweenShots;
+	public float timeBetweenShots = 0.5f;
+
+	public float dammage = 1.0f;
+
+	public float range  = 1.0f;
 
 
-	public float dammage;
+	private float _timer = 0;
 
-	private float _timer;
+
+
+	void OnTriggerEnter(Collider obj)
+	{
+		if (obj.tag == "Enemy")
+		{
+			enemies.Add(obj.gameObject);
+		}
+		Debug.Log("boop");
+	}
+
+
+	void OnTriggerExit(Collider obj)
+	{
+		if (obj.tag == "Enemy")
+		{
+			enemies.Remove(obj.gameObject);
+		}
+		Debug.Log("poob");
+	}
+
+	void Fire()
+	{
+
+
+	}
+
+	void StartRound()
+	{
+		colider.enabled = true;
+	}
+
+	void endrouind()
+	{
+		colider.enabled = false;
+	}
 
 
 	// Use this for initialization
-	void Start () {
-
-	
+	void Start () 
+	{
+	  colider = this.gameObject.GetComponent<Collider>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+
 	
 	}
 }
